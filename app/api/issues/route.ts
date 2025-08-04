59b7117b715e1dd56d15723a7078ec39eda8deeb
Adriana Cividini <dry.cividini@gmail.com>
2025-08-04 17:12:12 +0200

    handler error.

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -11,8 +11,9 @@ const createIssueSchema = z.object({
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
+
   if (!validation.success) {
-    return NextResponse.json(validation.error.issues, { status: 400 });
+    return NextResponse.json(validation.error, { status: 400 });
   }
   const newIssue = await prisma.issue.create({
     data: {
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme accentColor="pink" radius="full">
+        <Theme accentColor="pink" radius="medium">
           <Navbar />
           <main>{children}</main>
         </Theme>
