ac78efffbb41752b7fb1d6161a76dbe2fda51990
Adriana Cividini <dry.cividini@gmail.com>
2025-08-05 17:15:12 +0200

    handle errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,19 +1,15 @@
 import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "../../generated/prisma";
-import { z } from "zod";
+import { createIssueSchema } from "../../validationSchema";
 
 const prisma = new PrismaClient();
 
-const createIssueSchema = z.object({
-  title: z.string().min(1).max(255),
-  description: z.string().min(1),
-});
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createIssueSchema.safeParse(body);
 
   if (!validation.success) {
-    return NextResponse.json(validation.error, { status: 400 });
+    return NextResponse.json(validation.error.message, { status: 400 });
   }
   const newIssue = await prisma.issue.create({
     data: {
